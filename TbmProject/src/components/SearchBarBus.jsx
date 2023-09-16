f23d72caaa701a0e59ed0216045739be513d0030
esence <edouard.sence@u-bordeaux.fr>
2023-09-16 11:44:24 +0200

    show stop bus direction and images

--- TbmProject/src/components/SearchBarBus.jsx
+++ TbmProject/src/components/SearchBarBus.jsx
@@ -1,21 +1,62 @@
-// import { useState } from 'react'
+import React, { useState } from 'react';
 // import reactLogo from '../assets/react.svg'
 // import viteLogo from '/vite.svg'
 import '../assets/Css/App.css'
-import '../assets/json/NouveauTbm.json'
+import NouveauTbmData from '../assets/json/NouveauTbm.json';
 
- 
+
 function SearchBarBus() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  function handleInputChange(event) {
+    setSearchTerm(event.target.value);
+  }
+  function handleSearch() {
+    const filteredResults = NouveauTbmData.filter((item) =>
+      item.libelle.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(filteredResults);
+  }
+
+
   return (
     <>
-        <div className="searchBar">
-            <input type="text" placeholder="Search" />
-            <button type="submit">Search</button>
-        </div>
+      <div className="searchBar">
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}  // Value is associated with the searchTerm state
+          onChange={handleInputChange} // onChange handler updates searchTerm
+        />
+
+        <button type="submit" onClick={handleSearch}>Search</button>
+
         <div className="listBus">
+          {searchResults.map((result) => (
+            <div key={result.numero}>
+              {result.transport?.["BUS"]?.[0]?.image && (
+                <img
+                  src={`/${result.transport[result.vehicule][0].image}`}
+                  alt="logo"
+                />)}
+              <p>
+                {result.transport?.BUS?.[0]?.destination_name && (
+                  <>
+                    {result.libelle} ({result.vehicule} {result.transport["BUS"][0].lineId}) - {result.transport["BUS"][0].destination_name}</>
+                )}
+              </p>
+              {/* Display other relevant information */}
+            </div>
+          ))}
         </div>
+      </div>
+
     </>
   )
 }
+
+
+
+
 
 export default SearchBarBus
