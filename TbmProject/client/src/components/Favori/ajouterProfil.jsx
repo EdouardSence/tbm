bcaf698b101db4c53c4a392e70cc4a62f4a9cd47
@ajessondanie <aurelien.jesson-daniel@etu.u-bordeaux.fr>
2023-11-27 22:00:51 +0100

    api pas dispo l'équipe

--- TbmProject/client/src/components/Favori/ajouterProfil.jsx
+++ TbmProject/client/src/components/Favori/ajouterProfil.jsx
@@ -11,17 +11,23 @@ function ListeUser() {
 
     const ajouterUser = async () => {
         try {
-            await axios.post(`/api/user/ajouter-user?nom=${searchValue}`);
-            toast.success('Le profile a bien été supprimé');
-            window.location.href = `/tbm/profile/${searchValue}`;
+            // Vérifier si l'API existe avant d'envoyer la requête
+            if (typeof axios.post === 'function') {
+                await axios.post(`/api/user/ajouter-user?nom=${searchValue}`);
+                toast.success('Le profile a bien été supprimé');
+                window.location.href = `/tbm/profile/${searchValue}`;
+            } else {
+                // L'API n'existe pas, afficher un message d'erreur
+                toast.error("Le back arrive l'équipe");
+            }
         } catch (error) {
             toast.error(error.response.data.message);
         }
-    }
+    };
 
     return (
         <>
-            <SearchBarBus onSearchInputChange={handleSearchInputChange} placeholder="Ajouter un profil"/>
+            <SearchBarBus onSearchInputChange={handleSearchInputChange} placeholder="Ajouter un profil" />
             <button onClick={ajouterUser}>Ajouter le profil</button>
             <Toaster
                 position="top-right"
